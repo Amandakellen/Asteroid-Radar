08bc6869666b6d578f7805cd8d0f3a48d82a54bb
Amanda Soares <amanda.soares@ifood.com.br>
2024-11-29 20:01:20 -0300

    add worker

--- app/src/main/java/com/example/asteroid/MainActivity.kt
+++ app/src/main/java/com/example/asteroid/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.asteroid.workers.scheduleAsteroidDownloadWorker
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        scheduleAsteroidDownloadWorker(applicationContext)
         setContentView(R.layout.activity_main)
     }
 }
